4d37e7f427a07fb981cf384c9dcecbb71f8eec2d
Cuongnguyen20001605 <nguyendaccuong16052000@gmail.com>
2020-08-25 23:45:13 +0700

    demo githup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+
+const app=express()
+
+
+app.set('view engine','ejs')
+app.set('views','./views')
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+const port=process.env.PORT||3000
+
+app.listen(port,()=>console.log('start server'))
