2050bdcc135f97657c8bbe10955def42fb692af4
Cuongnguyen20001605 <nguyendaccuong16052000@gmail.com>
2020-08-26 22:50:05 +0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app=express()
 app.set('view engine','ejs')
 app.set('views','./views')
 
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     res.render('index')
 })
